a1ed42435889f587179b0214ba11a891e0488108
Sergey Cherepnin <cherepnin.sv@gmail.com>
2022-12-16 20:07:19 +0300

    small changes

--- rest-service/src/main/java/com/cherepnin/service/FileService.java
+++ rest-service/src/main/java/com/cherepnin/service/FileService.java
@@ -4,7 +4,6 @@ import com.cherepnin.entity.AppDocument;
 import com.cherepnin.entity.AppPhoto;
 import com.cherepnin.entity.BinaryContent;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileService {
     AppDocument getDocument(String id);
