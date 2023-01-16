e0c8d10191a4be8eb94c3d95d5d49a48bde72176
Sergey Cherepnin <cherepnin.sv@gmail.com>
2023-01-16 22:56:14 +0300

    flyway migration added

--- node/src/main/java/com/cherepnin/configuration/RabbitConfiguration.java
+++ node/src/main/java/com/cherepnin/configuration/RabbitConfiguration.java
@@ -1,12 +1,9 @@
 package com.cherepnin.configuration;
 
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static com.cherepnin.model.RabbitQueue.*;
 
 @Configuration
 public class RabbitConfiguration {
